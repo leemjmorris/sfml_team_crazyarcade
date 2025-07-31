ee76ef51270dc3c1c86088d06ad019fad26b2b3a
이명진 <happy_9586@naver.com>
2025-08-01 08:24:50 +0900

    Working on Xbox controller support.

--- sfml-crazyarcade/SceneDemo.cpp
+++ sfml-crazyarcade/SceneDemo.cpp
@@ -161,9 +161,6 @@ void SceneDemo::Enter()
 	bazzi->SetPosition(Utils::GetPlayerSpawnPoint(0));	
 	dao->SetPosition(Utils::GetPlayerSpawnPoint(1));
 
-	std::cout << "Player 1 position: (" << bazzi->GetPosition().x << ", " << bazzi->GetPosition().y << ")" << std::endl;
-	std::cout << "Player 2 position: (" << dao->GetPosition().x	<< ", " << dao->GetPosition().y << ")" << std::endl;
-
 	goReadyRoom = false;
 	bazzi->SetEnter(true);
 	dao->SetEnter(true);
