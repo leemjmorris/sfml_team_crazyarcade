8e0728b2e8c43907ec70cc33d35748c56a641331
김혜인 <rjqnrdl7524@naver.com>
2025-07-31 14:24:16 +0900

    화면 밖으로 블록 밀리지 않는 로직 수정
    기존 windowSize → Map size로 변경

--- sfml-crazyarcade/Block.cpp
+++ sfml-crazyarcade/Block.cpp
@@ -476,7 +476,6 @@ bool Block::IsBlockedAtTarget()
 {
     Scene* curScene = SCENE_MGR.GetCurrentScene();
 
-    sf::FloatRect windowBounds = FRAMEWORK.GetWindowBounds();
     sf::FloatRect splashBounds = GetHitBox().rect.getGlobalBounds();
 
     sprite.setPosition(targetPos);
@@ -489,7 +488,10 @@ bool Block::IsBlockedAtTarget()
     };
 
     // KHI: Window
-    if (!windowBounds.contains(targetPosCenter))
+    constexpr int GRID_SIZE = 52;
+    const sf::FloatRect worldBounds(0.f, 0.f, 15 * GRID_SIZE, 13 * GRID_SIZE);
+
+    if (!worldBounds.contains(targetPosCenter))
     {
         return true;
     }
